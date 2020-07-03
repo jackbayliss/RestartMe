d129b66ca4d009e709610c8cb4f8904e43a0e7c5
Jack Bayliss <jack@jackbayliss.com>
2020-07-03 01:28:47 +0100

    Inital commit
    Initial Commit

---
+++ RestartMe.py
@@ -0,0 +1,27 @@
+"""
+Auto open specified item on close
+Creative Commons Attribution-NonCommercial 4.0 International License
+Author - Jack Bayliss 03/07/2020
+"""
+import subprocess,platform,time,configparser
+from subprocess import CREATE_NEW_CONSOLE
+
+class RestartMe:
+ def __init__(self):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    self.Handler(config['DEFAULT']['target'])
+    
+ def Handler(self,file):
+    # Open file subprocess in a new window.
+    p = subprocess.Popen(file,creationflags=CREATE_NEW_CONSOLE)
+    poll = p.poll()
+    # While the poll is None, it's fine- no don't restart.
+    while p.poll() is None:
+        # Sleep to ensure we're not throttling the CPU.
+        time.sleep(1)
+    else:
+        # Restart the process
+        RestartMe()
+
+RestartMe()
